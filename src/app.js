c3cebcaec2cbe172fa51967795ec34016cef1c3c
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-23 09:41:19 +0700

    sads

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './components/List';
-import insertNote from './api/insertNote';
-
-insertNote('Chao cac ban ????   !')
-.then(a => a.text())
-.then(a => console.log(a))
-.catch(err => console.log(err));
-
 
 ReactDOM.render(
     <List />,
