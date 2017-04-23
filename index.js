ee009c4b3606cd7de601599aae91a40d21e7d4cf
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-23 09:12:53 +0700

    route post

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
-const { getAllNote } = require('./db');
+const parser = require('body-parser').urlencoded({ extended: false });
+const jsonParser = require('body-parser').json();
+
+const { getAllNote, insertNote } = require('./db');
 
 const app = express();
 app.listen(3000, () => console.log('Server started'));
@@ -15,4 +18,11 @@ app.get('/all', (req, res) => {
     getAllNote()
     .then(notes => res.send(notes))
     .catch(err => res.send(err));
+});
+
+app.post('/insert', jsonParser, (req, res) => {
+    const { note } = req.body;
+    insertNote(note)
+    .then(result => res.send('THANH_CONG'))
+    .catch(err => res.send('THAT_BAI'));
 });
